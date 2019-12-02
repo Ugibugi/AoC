22a3a1774f3723dc70c02dff0d6af2c1753e6184
Ugibugi <radek101998@interia.pl>
2019-12-02 14:38:38 +0100

    day 2

---
+++ 2019/advent2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include "intcode.hpp"
+
+int main()
+{
+    std::ifstream file("in");
+    char buff[255];
+    std::vector<int> mem;
+    mem.reserve(1000);
+    while (file.getline(buff, 255, ','))
+    {
+        mem.push_back(std::atoi(buff));
+    }
+    Intcode prog(mem);
+    for(int a=0;a<100;a++)
+    {
+        for(int b=0;b<100;b++)
+        {
+            mem[1] = a;
+            mem[2] = b;
+            prog.reset(mem);
+            if(prog.run() == 19690720)
+            {
+                std::cout << "a= " << a <<"\nb= " << b <<'\n';
+            }
+        }
+    }
+    return 0;
+}
---
+++ 2019/intcode.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+class Intcode
+{
+public:
+    std::vector<int>mem;
+    int ip=0;
+
+    Intcode(std::string filename)
+    {
+        std::ifstream file("in");
+        char buff[255];
+        mem.reserve(1000);
+        while (file.getline(buff, 255, ','))
+        {
+            mem.push_back(std::atoi(buff));
+        }
+    }
+    Intcode(const std::vector<int>& prog) : mem(prog){}
+    void reset(const std::vector<int>& prog)
+    {
+        mem = prog;
+        ip=0;
+    }
+    
+    int run(int retIndex=0)
+    {
+        while(true)
+        {
+            switch(mem[ip])
+            {
+            case 1:
+                mem[mem[ip + 3]] = mem[mem[ip + 1]] + mem[mem[ip + 2]];
+                ip+=4;
+                break;
+            case 2:
+                mem[mem[ip + 3]] = mem[mem[ip + 1]] + mem[mem[ip + 2]];
+                ip+=4;
+                break;
+            case 99:
+                ip++;
+                return mem[retIndex];
+            default:
+                std::cout << "error" << std::endl;
+                return -1;
+            }     
+        }  
+    }
+};
